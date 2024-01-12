8eaaa4e88c611d068820f1d62282398de0030685
weilinquan <352977670@qq.com>
2024-01-12 11:04:37 +0000

    add make prefill graph

--- frontend/Python/graph/kvcache/llama_config.py
+++ frontend/Python/graph/kvcache/llama_config.py
@@ -1,4 +1,6 @@
 from typing import List
+
+from ..graph import Graph
 from .config import *
 from ..op_def import *
 
@@ -24,9 +26,56 @@ class Llama7BMLPConfig(MLPConfig):
     def __init__(self) -> None:
         super().__init__()
         self.hidden_size = [11008, 11008, 4096]
-        self.pattern = [MatmulOp(), MatmulOp()]
+        self.pattern = [MatmulOp(), MatmulOp(), MatmulOp()]
 
 
 class Llama7BModelConfig(ModelConfig):
     def __init__(self) -> None:
         self.pattern = [Llama7BAttentionConfig(), Llama7BMLPConfig()] * 32
+
+    def verify_pattern(self, graph: Graph):
+        graph_op_idx = 0
+        marked_op_classes = set()
+        for config in self.pattern:
+            for op in config.pattern:
+                marked_op_classes.add(op.__class__)
+        for config_idx, config in enumerate(self.pattern):
+            assert isinstance(config, Config)
+            last_matmul_idx = 0
+            for op_idx, op in enumerate(config.pattern):
+                assert isinstance(op, Op)
+                graph_op = graph._body[graph_op_idx]
+                while graph_op.__class__ != op.__class__:
+                    assert (
+                        graph_op.__class__ not in marked_op_classes
+                    ), "Doesn't match {} vs {}".format(
+                        graph_op.__class__, op.__class__
+                    )
+                    graph_op_idx += 1
+                    graph_op = graph._body[graph_op_idx]
+                if isinstance(op, MatmulOp):
+                    assert (
+                        graph_op.tensor_meta["shape"][-1]
+                        == config.hidden_size[last_matmul_idx]
+                    ), "op{} in graph can't match config{}'s op{} hidden size {} vs {}".format(
+                        graph_op_idx,
+                        config_idx,
+                        op_idx,
+                        graph_op.tensor_meta["shape"][-1],
+                        config.hidden_size[last_matmul_idx],
+                    )
+                    last_matmul_idx += 1
+                    graph_op_idx += 1
+                elif isinstance(op, BatchMatmulOp):
+                    assert (
+                        graph_op.tensor_meta["shape"][0]
+                        == config.num_attention_heads
+                    ), "op{} in graph can't match config{}'s op{} num_heads {} vs {}".format(
+                        graph_op_idx,
+                        config_idx,
+                        op_idx,
+                        graph_op.tensor_meta["shape"][0],
+                        config.num_attention_heads,
+                    )
+
+                    graph_op_idx += 1
--- frontend/Python/graph/transform/special_opt_pattern.py
+++ frontend/Python/graph/transform/special_opt_pattern.py
@@ -1,23 +1,40 @@
 from ..kvcache import ModelConfig, Config, AttentionConfig
 from .. import Graph
-from .. import Op, MatmulOp, BatchMatmulOp
+from .. import Op, MatmulOp, BatchMatmulOp, CloneOp
+from typing import List
+from copy import deepcopy
 
 def kv_pattern_match(model_config: ModelConfig, graph: Graph):
     kv_ops = []
     graph_op_idx = 0
     for config_idx, config in enumerate(model_config.pattern):
-        if not isinstance(config, AttentionConfig):
-            continue
         for op_idx, op in enumerate(config.pattern):
             while graph._body[graph_op_idx].__class__ != op.__class__:
                 graph_op_idx += 1
+            if not hasattr(config, 'kv_op_idx'):
+                graph_op_idx += 1
+                continue
             if op_idx in config.kv_op_idx:
-                kv_ops.append(graph._body[graph_op_idx])
+                kv_ops.append(graph._body[graph_op_idx].name)
                 graph_op_idx += 1
                 continue
             graph_op_idx += 1
     
     return kv_ops
+
+def make_prefill_graph(graph: Graph, kv_ops: List[str]):
+    prefill_graph = deepcopy(graph)
+    prefill_graph._func_name = "prefill_forward"
+    setattr(prefill_graph, "kv_cache", [])
+    return_op = prefill_graph._body[-1]
+    for i, op in enumerate(prefill_graph._body):
+        if op.name in kv_ops:
+            cache_op = CloneOp.fx_create_node("kv_cache"+str(len(prefill_graph._body)), [op.name], [return_op.name], op.tensor_meta["shape"], op.tensor_meta["dtype"])
+            prefill_graph._body.insert(i+1, cache_op)
+            return_op.add_argument(cache_op.name)
+            return_op.add_parent(cache_op.name)
+            op.add_children(cache_op.name)
+            prefill_graph.kv_cache.append(cache_op.name)
 
 def apply_kv_cache(model_config: ModelConfig):
     origin_graph = None
@@ -27,8 +44,7 @@ def apply_kv_cache(model_config: ModelConfig):
         model_config.verify_pattern(graph)
     
         kv_ops = kv_pattern_match(model_config, origin_graph)
-        for op in kv_ops:
-            print(op.__dict__)
+        
 
 
     return verify_and_apply
